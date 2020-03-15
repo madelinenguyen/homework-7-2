c85440e5d613f14b2f85ca9cf8746da632b32766
madelinenguyen <maddynnguyen@gmail.com>
2020-03-14 23:47:11 -0700

    finished problem 2

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,40 +1,151 @@
+import java.util.Arrays;
+
 public class MaxHeap implements Heap {
-    int size;
+	int size;
 
-    Integer[] data;
+	Integer[] data;
 
-    public MaxHeap(int capacity) {
-        data = new Integer[capacity];
-        size = 0;
+	public MaxHeap(int capacity) {
+		data = new Integer[capacity];
+		size = 0;
+	}
+	
+	/*
+	 * I used this website to help me:
+	 * https://www.geeksforgeeks.org/max-heap-in-java/
+	 */
+	
+	// build a heap based on data
+	// to be implemented in O(nlogn)
+	public void MaxHeapLogN(Integer[] data) {
+		int start = (data.length/2)-1;
+		for (int i = start; i>=0; i--) {
+			heapify(data, data.length, i);
+		}
+	}
+
+	static void heapify(Integer arr[], int n, int i) {
+		int largest = i;
+		int left = 2 * i + 1;
+		int right = 2 * i + 2;
+
+		// left leaf > root
+		if (left < n && arr[left] > arr[largest])
+			largest = left;
+
+		// right leaf > root
+		if (right < n && arr[right] > arr[largest])
+			largest = right;
+
+		if (largest != i) {
+			int swap = arr[i];
+			arr[i] = arr[largest];
+			arr[largest] = swap;
+
+			heapify(arr, n, largest);
+		}
+	}
+
+	// build a heap based on data
+	// to be implemented in O(n)
+	public void MaxHeapN(Integer[] data) {
+		// copy data into heap
+		this.data = Arrays.copyOf(data, data.length);
+		
+		this.size = data.length;
+		int lastPar = parent(size-1);
+		for (int i = lastPar; i>=0; i--) heapDown(i);
+	}
+
+	// add an item to the heap
+	public boolean add(Integer item) {
+		// check size
+		if (size>=data.length) return false;
+		
+		// set item
+		data[size] = item;
+		size++;
+		
+		// call recursive heap up
+		heapUp(size-1);
+		return true;
+	}
+
+	// recursive heap up
+	private void heapUp(int n) {
+		if (n==0)
+			return;
+		Integer parent = (n-1)/2;
+		if (data[n]>data[parent]) {
+			swap(n, parent);
+			heapUp(parent);
+		}
+	}
+
+	// recursive heap down
+	private void heapDown(int n) {
+        if (n==size) return;
+        
+        Integer left = 2*n+1;
+        Integer right = 2*n+2;
+        
+        if (right <= size-1) {
+        	// parent vs. left leaf
+            if (data[n] < data[left]) {
+            	// parent vs. right leaf
+                if (data[left] >= data[right]) {
+                    swap(n, left);
+                    heapDown(left);
+                }
+                else {
+                	swap(n, right);
+                    heapDown(right);
+                }
+            }
+            else if (data[n] < data[right]) {
+            	swap(n, right);
+                heapDown(right);
+            }
+        }
     }
 
-    // build a heap based on data
-    // to be implemented in O(nlogn)
-    public void MaxHeapLogN(Integer[] data) {
-        // homework
-    }
+	// Returns position of parent
+	private int parent(int pos) {
+		return pos / 2;
+	}
 
-    // build a heap based on data
-    // to be implemented in O(n)
-    public void MaxHeapN(Integer[] data) {
-        // homework
-    }
+	public Integer pop() {
+		// check size
+		if (size<1) return null;
+		
+		int max = data[0];
+		data[0] = data[size - 1];
+		
+		// call recursive heap down
+		heapDown(0);
+		size--;
+		return max;
+	}
 
-    // add an item to the heap
-    public boolean add(Integer item) {
-        // homework
-        return false;
-    }
+	private void swap(int pos1, int pos2) {
+		int temp = data[pos1];
+		data[pos1] = data[pos2];
+		data[pos2] = temp;
+	}
 
-    // return the max item in the heap
-    public Integer get() {
-        // homework
-        return null;
-    }
+	// return the max item in the heap
+	public Integer get() {
+		// check if heap is empty
+		if (size==0)
+			return null;
+		return data[0];
+	}
 
-    // remove the root item
-    public Integer pop() {
-        // homework
-        return null;
-    }
+	// used to compare in J-Unit tests
+	public boolean equal(Integer[] data) {
+		for (int i=0; i<this.size; i++) {
+			if (this.data[i]!=data[i]) return false;
+		}
+		return true;
+	}
 }
--- src/MaxHeapTest.java
+++ src/MaxHeapTest.java
@@ -1,5 +1,172 @@
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class MaxHeapTest {
-    // homework
+
+	@Test
+	public void buildMaxHeapNLogN() {
+		/*
+		Integer[][] inputs = {
+				{},
+				{1, 1, 1},
+				{1, 4, 2, 7, 3},
+				{1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17}
+		};
+		
+		Integer[][] expects = {
+				{},
+				{1, 1, 1},
+				{7, 4, 2, 1, 3},
+				{17, 15, 13, 9, 6, 5, 10, 4, 8, 3, 1}
+		};
+		
+		for (int i=0; i<inputs.length; i++) {
+			MaxHeap testHeap = new MaxHeap(9);
+			testHeap.MaxHeapLogN(inputs[i]);
+			assertTrue(testHeap.equal(expects[i]));
+		}
+		*/
+		
+		MaxHeap emptyHeap = new MaxHeap(5);
+		Integer[] emptyInput = {};
+		emptyHeap.MaxHeapN(emptyInput);
+        assertTrue(emptyHeap.equal(emptyInput));
+        
+        MaxHeap testHeap1 = new MaxHeap(10);
+        Integer[] input1 = {1, 4, 2, 7, 3};
+        Integer[] answer1 = {7, 4, 2, 1, 3};
+        testHeap1.MaxHeapN(input1);
+        assertTrue(testHeap1.equal(answer1));
+
+        MaxHeap testHeap2 = new MaxHeap(10);
+        Integer[] input2 = {1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17};
+        Integer[] answer2 =  {17, 15, 13, 9, 6, 5, 10, 4, 8, 3, 1};
+        testHeap2.MaxHeapN(input2);
+        assertTrue(testHeap2.equal(answer2));
+        
+        MaxHeap testHeap3 = new MaxHeap(10);
+        Integer[] input3 = {1, 1, 1};
+        Integer[] answer3 = {1, 1, 1};
+        testHeap3.MaxHeapN(input3);
+        assertTrue(testHeap3.equal(answer3));
+	}
+
+	@Test
+	public void buildMaxHeapN() {
+		MaxHeap emptyHeap = new MaxHeap(5);
+		Integer[] emptyInput = {};
+		emptyHeap.MaxHeapN(emptyInput);
+        assertTrue(emptyHeap.equal(emptyInput));
+        
+        MaxHeap testHeap1 = new MaxHeap(10);
+        Integer[] input1 = {1, 4, 2, 7, 3};
+        Integer[] answer1 = {7, 4, 2, 1, 3};
+        testHeap1.MaxHeapN(input1);
+        assertTrue(testHeap1.equal(answer1));
+
+        MaxHeap testHeap2 = new MaxHeap(10);
+        Integer[] input2 = {1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17};
+        Integer[] answer2 =  {17, 15, 13, 9, 6, 5, 10, 4, 8, 3, 1};
+        testHeap2.MaxHeapN(input2);
+        assertTrue(testHeap2.equal(answer2));
+        
+        MaxHeap testHeap3 = new MaxHeap(10);
+        Integer[] input3 = {1, 1, 1};
+        Integer[] answer3 = {1, 1, 1};
+        testHeap3.MaxHeapN(input3);
+        assertTrue(testHeap3.equal(answer3));
+	}
+
+	@Test
+	public void addTest() {
+
+		MaxHeap heap = new MaxHeap(5);
+		Integer[][] expects = { {}, { 1 }, { 4, 1 }, { 4, 1, 2 }, { 7, 4, 2, 1 }, { 7, 4, 2, 1, 3 } };
+
+		/*
+		 * Integer[] valuesToAdd = {1, 4, 2, 7, 3};
+		 * 
+		 * for (int i=0; i<expects.length; i++) { if (i>=1)
+		 * assertTrue(heap.add(valuesToAdd[i])); assertTrue(heap.equal(expects[i]));
+		 * heap.print();
+		 * 
+		 * }
+		 */
+
+		MaxHeap testHeap = new MaxHeap(5);
+		assertTrue(testHeap.equal(expects[0]));
+
+		assertTrue(testHeap.add(1));
+		assertTrue(testHeap.equal(expects[1]));
+
+		assertTrue(testHeap.add(4));
+		assertTrue(testHeap.equal(expects[2]));
+
+		assertTrue(testHeap.add(2));
+		assertTrue(testHeap.equal(expects[3]));
+
+		assertTrue(testHeap.add(7));
+		assertTrue(testHeap.equal(expects[4]));
+
+		assertTrue(testHeap.add(3));
+		assertTrue(testHeap.equal(expects[5]));
+
+		assertFalse(testHeap.add(123));
+
+	}
+
+	@Test
+	public void getTest() {
+		MaxHeap testHeap = new MaxHeap(7);
+
+		assertEquals(null, testHeap.get());
+
+		// add values
+		testHeap.add(1);
+		testHeap.add(2);
+		testHeap.add(5);
+
+		// must type cast to Integer object, otherwise can't read
+		assertEquals((Integer) 5, testHeap.get());
+		testHeap.add(4);
+
+		assertEquals((Integer) 5, testHeap.get());
+		testHeap.add(123);
+
+		assertEquals((Integer) 123, testHeap.get());
+		testHeap.add(-500);
+
+		assertEquals((Integer) 123, testHeap.get());
+
+	}
+
+	@Test
+	public void popTest() {
+		MaxHeap testHeap = new MaxHeap(7);
+		testHeap.add(1);
+		assertEquals((Integer) 1, testHeap.pop());
+
+		// remove again, should be null
+		assertEquals(null, testHeap.pop());
+
+		// add more values
+		testHeap.add(6);
+		testHeap.add(4);
+
+		assertEquals((Integer) 6, testHeap.pop());
+		assertEquals((Integer) 4, testHeap.pop());
+
+		testHeap.add(2);
+		testHeap.add(5);
+		testHeap.add(99);
+		testHeap.add(501);
+
+		assertEquals((Integer) 501, testHeap.pop());
+
+		testHeap.pop();
+
+		assertEquals((Integer) 5, testHeap.pop());
+	}
+
 }
